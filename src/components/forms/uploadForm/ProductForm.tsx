7ce3669cf92d7ccd90ef577f7120ddace6bda3ca
chetanCronbay <chetan@cronbay-tech.com>
2025-08-29 12:37:26 +0530

    p

--- src/components/forms/uploadForm/ProductForm.tsx
+++ src/components/forms/uploadForm/ProductForm.tsx
@@ -497,8 +497,8 @@ export default function ProductForm({ product }: ProductFormProps) {
   }, [productName, productManufacturer, productModel]);
 
   return (
-    <div className="overflow-auto bg-white">
-      <div className="max-w-md mx-auto bg-white">
+    <div className="overflow-auto bg-white w-full">
+      <div className=" mx-auto bg-white">
         {/* Header */}
         <div className="flex items-center justify-between p-4 border-b">
           <h1 className="text-lg font-semibold text-gray-900">Add Product</h1>
