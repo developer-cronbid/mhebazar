d506ebebe42ca97970965dd59640f5e990058806
sumancron <suman@cronbaytechnologies.com>
2025-09-11 18:33:12 +0530

    b

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,6 @@
 import React, { Suspense } from "react";
 import BlogListClient from "./BlogListClient";
 import { Loader2 } from "lucide-react";
-import { motion as m } from "framer-motion";
 
 const BlogListPage = async ({
   searchParams,
@@ -15,13 +14,9 @@ const BlogListPage = async ({
   return (
     <Suspense
       fallback={
-        <m.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          className="min-h-screen bg-white flex items-center justify-center p-6"
-        >
+        <div className="min-h-screen bg-white flex items-center justify-center p-6 animate-fadeIn">
           <Loader2 className="h-16 w-16 text-[#5ca131] animate-spin" />
-        </m.div>
+        </div>
       }
     >
       <BlogListClient 
