c3457988ef89e5f3b1e6e67fbbe75d3721a8b159
nandhish-cron <nandhishwaran@cronbaytechnologies.com>
2026-03-02 16:28:37 +0530

    eeee

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,10 +1,12 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
  
+export const runtime = "edge";
+ 
 export function proxy(request: NextRequest) {
   return NextResponse.next();
 }
  
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/:path*"],
 };
